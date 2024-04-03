dd31754c0a93e445ec860dcf246e3adf772713ce
Filipe Holanda <filipemholanda@hotmail.com>
2024-04-03 19:19:01 -0300

    develop

--- shell.js
+++ shell.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /**
  * ==== wwebjs-shell ====
  * Used for quickly testing library features
@@ -6,32 +7,62 @@
  * and then drop you into Node REPL with `client` in its context.
  */
 
-const repl = require("repl");
+const repl = require('repl');
 
-const { Client, LocalAuth } = require("./index");
+const { Client, LocalAuth, MessageMedia } = require('./index');
+
+const executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 
 const client = new Client({
-    puppeteer: { headless: false },
+    puppeteer: { headless: false,
+        executablePath: executablePath,
+        args: [
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-setuid-sandbox',
+            '--no-first-run',
+            '--no-sandbox',
+            '--no-zygote',
+            '--deterministic-fetch',
+            '--disable-features=IsolateOrigins',
+            '--disable-site-isolation-trials',
+        ],
+    },
     authStrategy: new LocalAuth(),
 });
 
-console.log("Initializing...");
+console.log('Initializing...');
 
 client.initialize();
 
-client.on("qr", () => {
-    console.log("Please scan the QR code on the browser.");
+client.on('qr', () => {
+    console.log('Please scan the QR code on the browser.');
 });
 
-client.on("authenticated", (session) => {
+client.on('authenticated', (session) => {
     console.log(JSON.stringify(session));
 });
 
-client.on("ready", () => {
-    const shell = repl.start("wwebjs> ");
+client.on('ready', () => {
+    const shell = repl.start('wwebjs> ');
     shell.context.client = client;
-    client.sendMessage("5511988131269@c.us", "olá");
-    shell.on("exit", async () => {
+
+    const to = '5511988131269@c.us';
+    const video = {
+        to: to,
+        from: '551156796980',
+        type: 'video',
+        url: 'http://localhost:8000/whatsapp/communication/file/5cb3c779-36e3-45a8-b2d6-aa4a733e048a.stp',
+        caption: '',
+        filename: 'lalala.mp4'
+    };
+    MessageMedia.fromUrl(video.url, {unsafeMime: true}).then((media) => {
+        console.log('media', media);
+        client.sendMessage(video.to, media, {sendMediaAsDocument: true, caption: video.caption });
+        client.sendMessage(to, 'olá');
+    });
+      
+    shell.on('exit', async () => {
         await client.destroy();
     });
 });
